330e2c043e454906387268dd5660332988b04f3b
Hamed Ahadi <info.shahdokht@gmail.com>
2022-01-18 19:32:37 +0330

    Comment model and migration

---
+++ app/Models/Comment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'full_name', 'text', 'mother_id'
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo('App\Models\Comment', 'mother_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany('App\Models\Comment', 'mother_id');
+    }
+}
